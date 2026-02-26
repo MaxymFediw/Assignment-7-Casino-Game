e288ff0d4fa56e6b2e13e18635dbeb6e369ab80f
Maxym Fediw <362706764@GoTVDSB.ca>
2026-02-26 13:18:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment_7_Casino_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Maxym F.
+
+            Part1();
+
+
+        }
+        public static void Part1() 
+        {
+           
+            
+            
+            Random generator = new Random();
+            
+            int score, coinSide, tries;
+            
+            coinSide = generator.Next(1, 3);
+
+            Console.WriteLine("Heads or Tails?");
+            
+
+            if (coinSide == 1) 
+            {
+                Console.WriteLine("Heads!");
+            }
+
+            if (coinSide == 2) 
+            {
+                Console.WriteLine("Tails!");
+            }
+        
+        
+        
+        
+        }
+    }  
+}
