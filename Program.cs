b506966498ebcc4c8045746f1257d504a6405f6a
Maxym Fediw <362706764@GoTVDSB.ca>
2026-02-27 13:04:55 -0500

    Checkpoint #2

--- Program.cs
+++ Program.cs
@@ -10,30 +10,91 @@
 
 
         }
-        public static void Part1() 
+        public static void Part1()
         {
-           
-            
-            
+
+
+
             Random generator = new Random();
-            
-            int score, coinSide, tries;
-            
+
+            int points, coinSide, tries, heads, tails;
+            string guess, keepGoing;
+
             coinSide = generator.Next(1, 3);
 
-            Console.WriteLine("Heads or Tails?");
-            
-            
-            if (coinSide == 1) 
-            {
-                Console.WriteLine("Heads!");
-            }
+            bool done = false;
 
-            if (coinSide == 2) 
+            Console.WriteLine("You start with 3 points. You lose one point every time you guess wrong, and you gain a point every guess thats correct. If you have 0 points, it's game over. So, Heads or Tails?");
+            guess =Console.ReadLine().ToLower();
+
+            //heads = 1;
+            //tails = 2;
+
+            points = 3;
+            
+            
+            
+            while (!done)
             {
-                Console.WriteLine("Tails!");
+
+                coinSide = generator.Next(1, 3)
+
+                done = true;
+                
+                if (guess == "heads" && coinSide == 1)
+                {
+                    Console.WriteLine("Heads!");
+                    Console.WriteLine("Correct!");
+                    points++;
+
+                }
+
+                if (guess == "tails" && coinSide == 2)
+                {
+                    Console.WriteLine("Tails!");
+                    Console.WriteLine("Correct!");
+                    points++;
+                }
+
+                if (guess == "heads" && coinSide != 1) 
+                {
+                    Console.WriteLine("Tails!");
+                    Console.WriteLine("Your Guess Was Inorrect!");
+                    points--;
+                }
+
+                if (guess == "tails" && coinSide != 2) 
+                { 
+                    Console.WriteLine("Heads!");
+                    Console.WriteLine("Your guess was Incorrect!");
+                    points--;
+                }
+               
+                Console.WriteLine("You now have " + points + " points.");
+               
+                Console.WriteLine("Would you like to continue? Yes or No?");
+                keepGoing = Console.ReadLine().ToLower();
+
+                if (keepGoing == "no" || keepGoing == "No") 
+                {
+                    Console.WriteLine("Okay, Goodbye.");
+                    done = true;
+                }
+
+                if (keepGoing == "yes" || keepGoing == "Yes") 
+                {
+                    Console.WriteLine();
+                    done = false;
+                }
+
+                if (points == 0) 
+                {
+                    Console.WriteLine("You have 0 points remaining. You lose.");
+                    done = true;
+                }
+
+                
             }
-        
         
         
         
